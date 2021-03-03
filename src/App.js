af518d314a73652bd0be88c66b1bf2f4971d7769
macfrei <lene@7freiheiten.de>
2021-03-03 08:11:09 +0100

    Add Button and Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AppHeader from './components/AppHeader'
+import AppNavigation from './components/AppNavigation'
 
 function App() {
   return (
     <div>
       <AppHeader title="Favourite Wizard" />
+      <AppNavigation />
     </div>
   )
 }
