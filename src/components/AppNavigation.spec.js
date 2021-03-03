543caaf284c88ae180a9bdb09bb22438ec9d037a
macfrei <lene@7freiheiten.de>
2021-03-03 14:04:48 +0100

    Add test with MemoryRouter for Navigation

--- src/components/AppNavigation.spec.js
+++ src/components/AppNavigation.spec.js
@@ -1,5 +1,14 @@
+import { render, screen } from '@testing-library/react'
 import AppNavigation from './AppNavigation'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('AppNavigation', () => {
-  it.todo('should render a navigation with two links')
+  it('should render a navigation with two links', () => {
+    render(
+      <MemoryRouter>
+        <AppNavigation />
+      </MemoryRouter>
+    )
+    expect(screen.getAllByRole('link')).toHaveLength(2)
+  })
 })
